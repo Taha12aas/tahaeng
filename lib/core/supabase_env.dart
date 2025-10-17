befcd1d05c52772d3d7847b6e33d5d2d9d35a4c4
taha <asdfghmnt@gmail.com>
2025-10-17 18:55:09 +0300

    url

--- lib/core/supabase_env.dart
+++ lib/core/supabase_env.dart
@@ -1,5 +1,13 @@
 class SupaEnv {
-  static const url = 'https://iijuwfvyakftpkndyqzn.supabase.co';
+  static const url = 'https://lkiejkrivjfyumwynytd.supabase.co';
+  //https://iijuwfvyakftpkndyqzn.supabase.co ,,هاد قاعدة البيانات القديمة
+  //https://lkiejkrivjfyumwynytd.supabase.co ,,هاد قاعدة البيانات مستودع طريق حلب
   static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpanV3ZnZ5YWtmdHBrbmR5cXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MDA4ODcsImV4cCI6MjA2ODI3Njg4N30.ou02o28wsUV71B0B8FM4m4CsHgSlJ_ce3nhHFfi9_Zg';
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxraWVqa3JpdmpmeXVtd3lueXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MzEyOTMsImV4cCI6MjA3NjIwNzI5M30.OBMtBtc9zjDHTA68EjkYvXOgWyHgXL9fSVbp46UFaBc";
 }
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxraWVqa3JpdmpmeXVtd3lueXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MzEyOTMsImV4cCI6MjA3NjIwNzI5M30.OBMtBtc9zjDHTA68EjkYvXOgWyHgXL9fSVbp46UFaBc 
+//هاد الجديد طريق حلب
+
+//      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpanV3ZnZ5YWtmdHBrbmR5cXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MDA4ODcsImV4cCI6MjA2ODI3Njg4N30.ou02o28wsUV71B0B8FM4m4CsHgSlJ_ce3nhHFfi9_Zg';
+//هاد المشروع القديم
